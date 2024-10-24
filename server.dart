389968fcb254b6c921f16f1b49356543755d8fdb
TwoSich <example@example.com>
2024-10-24 12:07:39 -0300

    first commit

--- server.dart
+++ server.dart
@@ -1,14 +1,13 @@
 import 'dart:io';
 
 Future<void> main() async {
-
-  var server = await HttpServer.bind(
-    InternetAddress.anyIPv4, 
+  final server = await HttpServer.bind(
+    InternetAddress.anyIPv4,
     3000,
   );
   print('Server in http://${server.address.host}:${server.port}');
 
-  await for (HttpRequest request in server) {
+  await for (final HttpRequest request in server) {
     request.response
       ..headers.contentType = ContentType.text
       ..write('Hello World')

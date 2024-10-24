9869ab8fe921b3b38d195cb785779c64b44c6503
TwoSich <example@example.com>
2024-10-24 14:23:47 -0300

    added

--- routes/webhook.dart
+++ routes/webhook.dart
@@ -9,12 +9,15 @@ Future<Response> onRequest(RequestContext context) async {
     final body = await context.request.body();
     final data = jsonDecode(body);
 
+    // Verifica si hay adjuntos
     if (data['attachments'] == null) {
       return Response.json(
-          body: {'error': 'No hay contenido o adjuntos en el mensaje.'},
-          statusCode: 400);
+        body: {'error': 'No hay contenido o adjuntos en el mensaje.'},
+        statusCode: 400,
+      );
     }
 
+    // Filtra y extrae los enlaces de las imágenes
     final List<String> newImageLinks = (data['attachments'] as List)
         .where((attachment) =>
             attachment['content_type'] != null &&
@@ -22,6 +25,7 @@ Future<Response> onRequest(RequestContext context) async {
         .map((attachment) => attachment['url'].toString())
         .toList();
 
+    // Agrega los nuevos enlaces de imagen
     imageLinks.addAll(newImageLinks);
 
     if (newImageLinks.isNotEmpty) {
@@ -31,10 +35,16 @@ Future<Response> onRequest(RequestContext context) async {
     }
 
     return Response.json(
-        body: {'message': 'Recibido', 'image_links': newImageLinks});
+      body: {
+        'message': 'Recibido',
+        'image_links': newImageLinks,
+      },
+      statusCode: 200,
+    );
   }
 
   if (context.request.method == HttpMethod.get) {
+    // Retorna todos los enlaces de imágenes almacenados
     return Response.json(body: {'image_links': imageLinks});
   }
 

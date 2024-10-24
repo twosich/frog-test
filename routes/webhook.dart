a2f6a368b24c29901595a7bd8d3cfbc2911b6604
TwoSich <example@example.com>
2024-10-24 15:04:00 -0300

    change cors

--- routes/webhook.dart
+++ routes/webhook.dart
@@ -5,15 +5,29 @@ import 'package:dart_frog/dart_frog.dart';
 List<String> imageLinks = [];
 
 Future<Response> onRequest(RequestContext context) async {
+  // Configura los encabezados CORS
+  final corsHeaders = {
+    'Access-Control-Allow-Origin': '*', // Permitir todas las orígenes, puedes restringir esto si es necesario
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', // Métodos permitidos
+    'Access-Control-Allow-Headers': 'Content-Type' // Encabezados permitidos
+  };
+
+  if (context.request.method == HttpMethod.options) {
+    return Response(
+      statusCode: 204,
+      headers: corsHeaders,
+    );
+  }
+
   if (context.request.method == HttpMethod.post) {
     final body = await context.request.body();
     final data = jsonDecode(body);
 
-    // Asegúrate de que el body tenga el formato correcto
     if (data['attachments'] == null) {
       return Response.json(
           body: {'error': 'No hay contenido o adjuntos en el mensaje.'},
-          statusCode: 400);
+          statusCode: 400,
+          headers: corsHeaders);
     }
 
     final List<String> newImageLinks = (data['attachments'] as List)
@@ -23,7 +37,6 @@ Future<Response> onRequest(RequestContext context) async {
         .map((attachment) => attachment['url'].toString())
         .toList();
 
-    // Agrega las nuevas imágenes a la lista global
     imageLinks.addAll(newImageLinks);
 
     if (newImageLinks.isNotEmpty) {
@@ -33,13 +46,13 @@ Future<Response> onRequest(RequestContext context) async {
     }
 
     return Response.json(
-        body: {'message': 'Recibido', 'image_links': newImageLinks});
+        body: {'message': 'Recibido', 'image_links': newImageLinks},
+        headers: corsHeaders);
   }
 
-  // Manejo de la solicitud GET
   if (context.request.method == HttpMethod.get) {
-    return Response.json(body: {'image_links': imageLinks});
+    return Response.json(body: {'image_links': imageLinks}, headers: corsHeaders);
   }
 
-  return Response(body: 'Método no permitido', statusCode: 405);
+  return Response(body: 'Método no permitido', statusCode: 405, headers: corsHeaders);
 }

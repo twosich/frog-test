39f3e1a77f6c8ba5f3cb017496adfe59f3f57b49
TwoSich <example@example.com>
2024-10-24 13:52:14 -0300

    added images

--- routes/webhook.dart
+++ routes/webhook.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:dart_frog/dart_frog.dart';
 
+List<String> imageLinks =
+    []; // Lista para almacenar los enlaces de las imágenes
+
 Future<Response> onRequest(RequestContext context) async {
   if (context.request.method == HttpMethod.post) {
     final body = await context.request.body();
@@ -13,21 +16,29 @@ Future<Response> onRequest(RequestContext context) async {
           statusCode: 400);
     }
 
-    final List<String> imageLinks = (data['attachments'] as List)
+    // Filtrar enlaces de imágenes y agregar a la lista
+    final List<String> newImageLinks = (data['attachments'] as List)
         .where((attachment) =>
             attachment['content_type'] != null &&
             attachment['content_type'].toString().startsWith('image'))
         .map((attachment) => attachment['url'].toString())
         .toList();
 
-    if (imageLinks.isNotEmpty) {
-      print('Links de imágenes: $imageLinks');
+    imageLinks.addAll(newImageLinks); // Almacenar enlaces en la lista
+
+    if (newImageLinks.isNotEmpty) {
+      print('Links de imágenes: $newImageLinks');
     } else {
       print('No hay imágenes en este mensaje.');
     }
 
     return Response.json(
-        body: {'message': 'Recibido', 'image_links': imageLinks});
+        body: {'message': 'Recibido', 'image_links': newImageLinks});
+  }
+
+  // Manejar solicitudes GET para obtener los enlaces de las imágenes
+  if (context.request.method == HttpMethod.get) {
+    return Response.json(body: {'image_links': imageLinks});
   }
 
   return Response(body: 'Método no permitido', statusCode: 405);
